984f51ce33cbc2bbe409454de9f5d4f1b7e53ca3
TheDirector23 <70852990+TheDirector23@users.noreply.github.com>
2020-11-16 17:52:39 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -32,12 +32,12 @@ if((weatherForecastForTommorowNoon.pop >= .01) && (weatherForecastForTommorowNoo
 }
 
 // Beautiful day
-if((weatherForecastForTommorowNoon.main.feels_like >= 55 && weatherForecastForTommorowNoon.main.feels_like <= 85) && (weatherForecastForTommorowNoon.pop == 0) && (weatherForecastForTommorowNoon.main.humidity <= 50) && (weatherForecastForTommorowNoon.wind.speed <= 10)){
+if((weatherForecastForTommorowNoon.main.temp >= 55 && weatherForecastForTommorowNoon.main.temp <= 85) && (weatherForecastForTommorowNoon.pop == 0) && (weatherForecastForTommorowNoon.main.humidity <= 50) && (weatherForecastForTommorowNoon.wind.speed <= 10)){
   output = "It's going to be a beautiful day tomorrow! Don't stay inside if you don't have to! You could: <li>Ride a bike</li> <li>Go on a hike</li> <li>Go jogging/running</li> <li>Have a picnic</li> <li>Play a sport (tennis, soccer, football)</li> <li>Go cloudwatching</li> <li>Sketch or paint a picture of a landscape</li> <li>Take pictures outside</li> <li>Visit a site near you (like a historic site outdoors)</li> <li>Go downtown and walk around</li> <li>Chill in your backyard and enjoy the weather</li>";
 }
 
 // Wind
-if ((weatherForecastForTommorowNoon.main.feels_like >= 60 && weatherForecastForTommorowNoon.main.feels_like <= 90) && (weatherForecastForTommorowNoon.wind.speed >= 10 && weatherForecastForTommorowNoon.wind.speed <= 20) && (weatherForecastForTommorowNoon.main.humidity >= 0 && weatherForecastForTommorowNoon.main.humidity <= 85)){
+if ((weatherForecastForTommorowNoon.main.temp >= 60 && weatherForecastForTommorowNoon.main.temp <= 90) && (weatherForecastForTommorowNoon.wind.speed >= 10 && weatherForecastForTommorowNoon.wind.speed <= 20) && (weatherForecastForTommorowNoon.main.humidity >= 0 && weatherForecastForTommorowNoon.main.humidity <= 85)){
   output = "It's going to be a little windy tomorrow! You should: <li>Sail a boat</li> <li>Fly a kite</li> <li>Go windsurfing</li> <li>Go to a kite festival</li> <li>Fly a paper airplane outside</li> <li>Play with pinwheels and bubbles</li> <li>Set up and listen to a windchime</li> <li>Do a windy-day photoshoot</li> <li>Paint pottery</li> <li>Read <i>Winnie the Pooh and the Blustery Day</i></li>";
 }
 
@@ -51,19 +51,19 @@ if (weatherForecastForTommorowNoon.humidity >= 80){
 }
 
 // Constant condition
-if (weatherForecastForTommorowNoon.main.feels_like <= 30){
+if (weatherForecastForTommorowNoon.main.temp <= 30){
   output = "Tomorrow's a bit too cold for outdoor activities! Maybe you should: <li>Curl up with your favorite book or movie</li> <li>Bake warm brownies or congo bars</li> <li>Make/buy coffee or hot chocolate</li> <li>Create a tv show!</li> <li>Go shopping</li> <li>Do arts and crafts</li> <li>Make s'mores</li> <li>Spend the day in a cozy cafe</li> <li>Learn to juggle</li> <li>Make cutout snowflakes</li>";
 }
 
-if((weatherForecastForTommorowNoon.main.feels_like >= 31 && weatherForecastForTommorowNoon.main.feels_like <= 60) && weatherForecastForTommorowNoon.pop == 0){
+if((weatherForecastForTommorowNoon.main.temp >= 31 && weatherForecastForTommorowNoon.main.feels_like <= 60) && weatherForecastForTommorowNoon.pop == 0){
   output = "It's going to be a little chilly tomorrow. You could: <li>Take a walk</li> <li>Throw a baseball with friends/family</li> <li>Have a firepit outside and roast marshmallows</li> <li>Go downtown</li> <li>Go shopping</li> <li>Paint a picture or sketch</li> <li>Do a fun science experiment</li> <li>Make pizza</li> <li>Make decorations for the current season</li> <li>Felt your favorite animal (No clue what felting is? Google it!)</li>";
 }
 
-if((weatherForecastForTommorowNoon.main.feels_like >= 61 && weatherForecastForTommorowNoon.main.feels_like <= 79) && (weatherForecastForTommorowNoon.pop == 0)){
+if((weatherForecastForTommorowNoon.main.temp >= 61 && weatherForecastForTommorowNoon.main.feels_like <= 79) && (weatherForecastForTommorowNoon.pop == 0)){
   output = "It's going to be pretty nice tomorrow! You could: <li>Go jogging</li> <li>Take a bike ride</li> <li>Host an outdoor party</li> <li>Go downtown</li> <li>Do a photoshoot</li> <li>Read a book outside</li> <li>Visit a friend or family</li> <li>See if any fun events are happening near you!</li> <li>Film a movie</li> <li>Just relax...</li>";
 }
 
-if (weatherForecastForTommorowNoon.main.feels_like >= 80){
+if (weatherForecastForTommorowNoon.main.temp >= 80){
   output = "Tomorrow's a little too hot or humid for most outdoor activities! Maybe you should: <li>Go swimming at the pool</li> <li>Bake some sweets</li> <li>Make/buy refreshing lemonade</li> <li>Plan a party</li> <li>Play some games</li> <li>Write letters to a friend</li> <li>Help an elderly acquaintence</li> <li>Visit family or friends</li> <li>Bake cookies for your neighbors</li> <li>Sing songs or play an instrument or both!</li>";
 }
 
@@ -76,7 +76,7 @@ if (weatherForecastForTommorowNoon.main.feels_like >= 80){
       </p>
       <p>Location: ${weatherForecastCity.name}</p>
       <p>Description: ${weatherForecastForTommorowNoon.weather[0].description}</p>
-      <p>Feels like: ${weatherForecastForTommorowNoon.main.feels_like} F</p>
+      <p>Temperature: ${weatherForecastForTommorowNoon.main.temp} F</p>
       <p>Precipitation: ${weatherForecastForTommorowNoon.pop} inches</p>
       <p>Wind: ${weatherForecastForTommorowNoon.wind.speed} mph</p>
       `;
